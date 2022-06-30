7d31d9cab99884fa8b42af2121d070b3c3d3e60c
Your Name <you@example.com>
2022-06-30 15:38:42 -0700

    Added styling and Buttons/Filter Bar components

---
+++ src/components/QuestionAnswerBlock.js
@@ -0,0 +1,12 @@
+// import Question from "./Question";
+
+// const QuestionAnswerBlock = (props) => {
+//     return (
+//         <div className="question-answer">
+//             {props.questions.map(q => {
+//                 <Question
+//             })}
+//         </div>);
+// }
+
+// export default QuestionAnswerBlock;
